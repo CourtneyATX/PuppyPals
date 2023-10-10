f290e04b515ccbba4e6b8249c2e2f33469a61df8
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-10-10 15:20:39 -0500

    added featuredPup

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,36 @@ import {puppyList} from './data.js'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
 
   console.log("puppyList: ", puppyList);
+
+  // function handleClick() {
+
+  // }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
 
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
-          })
-        };
+            return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+              </p>
+          })}
 
+          {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+          )}
       </div>
     </>
   )
