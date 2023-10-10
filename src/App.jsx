477f3d36045db4d396e3bed644a07cfff3cbbadd
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-10-10 14:57:21 -0500

    updated state to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
+
 
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList)
 
   return (
     <>
       <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        };
+        
       </div>
     </>
   )
